8dbe98bcd282dd4d2ae246499a82d493b45e34bf
MJULIAN90 <michaeljulian50@gmail.com>
2021-09-02 02:15:23 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+//import {NavigationContainer} from '@react-navigation/native';
+//import Navigation from './src/navigation/Navigation';
+import {Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>hola</Text>
+    </View>
+    /*  <NavigationContainer>
+      <Navigation />
+    </NavigationContainer> */
+  );
+}
